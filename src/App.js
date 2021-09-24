44ae9fa3b84357f6f482bccd4940d81a292e769d
Egor <satyricons@yandex.ru>
2021-09-24 21:43:31 +0300

    position 7

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
 
-       <Head id="Егор Каменски"/>
+       <Head id="Егор Каменски" />
 
     </div>
   );
--- src/components/head/head.jsx
+++ src/components/head/head.jsx
@@ -6,12 +6,12 @@ const Head = (props) => {
     return (
        <div className={headcss.allhead}>
 
-           <div><img src={logo}></img></div>
+           <div><img src={logo} /></div>
            <div>
                <h1>Эндокринология для всех</h1>
                <p>Барановичская детская городская поликлиника</p>
            </div>
-           <div><a>225320 г.Барановичи, ул. Куйбышева, 46</a>
+           <div><p>225320 г.Барановичи, ул. Куйбышева, 46</p>
                <p>тел. +375 16 342-13-13</p></div>
            <div>
                CABINET
