43f5bfe8d7af0b447f36197cb60d6ac53495e5a6
Egor <satyricons@yandex.ru>
2021-09-27 15:15:57 +0300

    Make body 6

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ function App(props) {
 	return (
 		<BrowserRouter>
 			<div className="App">
-				<PreHead path={props.path}/>
-				<Head id="Егор Каменски"/>
+				<PreHead nameStat={props.nameStat} path={props.path}/>
+				<Head id={props.id}/>
 				<Route path="/spisok" component={Spisok}/>
-
 				<Route path={props.path[0]}> <Body naz={props.naz[0]} text={props.text[0]}/></Route>
 				<Route path={props.path[1]}> <Body naz={props.naz[1]} text={props.text[1]}/></Route>
 				<Route path={props.path[2]}> <Body naz={props.naz[2]} text={props.text[2]}/></Route>
--- src/components/prehead/prehead.jsx
+++ src/components/prehead/prehead.jsx
@@ -6,14 +6,13 @@ const PreHead = (props) => {
 	return (
 		<div className={prehead.allprehead}>
 			<ul className={prehead.ul}>
-				<li><NavLink className={preheadcss.item41} to={props.path[0]}>Полезная информация</NavLink></li>
-				<li><NavLink className={preheadcss.item41} to={props.path[1]}>Умные устройства</NavLink></li>
-				<li><NavLink className={preheadcss.item41} to={props.path[2]}>Где купить</NavLink></li>
-				<li><NavLink className={preheadcss.item41} to={props.path[3]}>Школа диабета</NavLink></li>
-				<li><NavLink className={preheadcss.item41} to={props.path[4]}>Считаем углеводы</NavLink></li>
+				<li><NavLink className={preheadcss.item41} to={props.path[0]}>{props.nameStat[0]}</NavLink></li>
+				<li><NavLink className={preheadcss.item41} to={props.path[1]}>{props.nameStat[1]}</NavLink></li>
+				<li><NavLink className={preheadcss.item41} to={props.path[2]}>{props.nameStat[2]}</NavLink></li>
+				<li><NavLink className={preheadcss.item41} to={props.path[3]}>{props.nameStat[3]}</NavLink></li>
+				<li><NavLink className={preheadcss.item41} to={props.path[4]}>{props.nameStat[4]}</NavLink></li>
 			</ul>
 		</div>
 	);
 }
-
 export default PreHead;
