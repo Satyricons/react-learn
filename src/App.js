588fa0ebdb5a56a02721d6521c3c451e68aae553
Egor <satyricons@yandex.ru>
2021-09-28 15:40:56 +0300

    Redux past 4

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import {Route} from "react-router-dom";
 function App(props) {
 	let bodyPage = props.state.bodyPage;
 	let userPage = props.state.userPage;
-	let allSpisok = bodyPage.naz.map((d, i) => <Route path={bodyPage.path[i]}><Body key={i} naz={bodyPage.naz[i]}
+	let allSpisok = bodyPage.naz.map((d, i) => <Route key={i} path={bodyPage.path[i]}><Body key={i} naz={bodyPage.naz[i]}
 																					text={bodyPage.text[i]}/></Route>);
 	return (
 		<div className="App">
 			<Menu bodyPage={bodyPage}/>
-			<Head userPage={userPage}/>
+			<Head userPage={userPage}><title>Диа</title></Head>
 			<Route path="/spisok" component={Spisok}/>
 			{allSpisok}
 		</div>
