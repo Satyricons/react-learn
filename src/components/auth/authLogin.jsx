b80c7f052582bd239afdb58a74502dcf291f6e2a
Egor <satyricons@yandex.ru>
2022-06-01 12:53:25 +0300

    react+api

---
+++ src/components/auth/authLogin.jsx
@@ -0,0 +1,54 @@
+import React, {useRef, useState} from "react"
+import headcss from "../head/head.module.css"
+import * as api from "../../api/api"
+
+const AuthLogin = (props) => {
+	const [modeRegister, setModeRegister] = useState(false)
+	let email = useRef();
+	let password = useRef();
+	let buttonAuth = () => {
+		api.getUserAuth(email.current.value, password.current.value).then(data => {
+				localStorage.setItem('myToken', data.authorizeToken)
+				props.setAuth(data.authorizeName, data.authorizePhoto, data.authorizeId, data.authorizeStatus, data.authorizeCountFollow)
+			}
+		)
+	}
+	let buttonRegister = () => {
+		api.setUserRegister(email.current.value, password.current.value).then(data => {
+			console.log(data)
+		})
+	}
+	return <div>
+		{!modeRegister ?
+			<div>
+				<h1>Авторизация</h1>
+				<p>Email:</p>
+				<p><input ref={email} type="text"/></p>
+				<p>Password:</p>
+				<p><input ref={password} type="text"/></p>
+				<button onClick={buttonAuth}>Войти</button>
+				<p className={headcss.item41}
+				   onClick={() => {
+					   setModeRegister(true)
+				   }}
+				>Регистрация </p>
+			</div>
+			:
+			<div>
+				<h1>Регистрация</h1>
+				<p>Email:</p>
+				<p><input ref={email} type="text"/></p>
+				<p>Password:</p>
+				<p><input ref={password} type="text"/></p>
+				<button onClick={buttonRegister}>OK</button>
+				<p className={headcss.item41}
+				   onClick={() => {
+					   setModeRegister(false)
+				   }}
+				>Авторизация </p>
+			</div>
+		}
+	</div>
+}
+
+export default AuthLogin
--- src/components/users/usersList.jsx
+++ src/components/users/usersList.jsx
@@ -1,172 +1,355 @@
-import React, {useEffect, useState} from "react"
+// import React, {useEffect, useState} from "react"
+// import usersCss from "./users.module.css"
+// import face from "../head/img/user.png"
+// import * as api from "../../api/api"
+//
+// const UsersList = (props) => {
+//
+// 	const [id, setId] = useState(undefined)
+// 	const [searchValue, setSearchValue] = useState("")
+// 	const [searchPeople, setSearchPeople] = useState([])
+// 	const [modeSearch, setModeSearch] = useState(true)
+//
+//
+// // эффект при вводе символа в поисковой строке:
+// 	useEffect(() => searchValue !== '' ?
+// 		api.getSearchUsers(1, searchValue).then(data => {
+// 			setSearchPeople(data.newUsers)
+// 			data.newUsers.length === 0 ? setModeSearch(false) : setModeSearch(true)
+// 		})
+// 		:
+// 		setModeSearch(false), [searchValue])
+//
+// 	return <div>
+//
+// 		{/*строка поиска*/}
+// 		<input onChange={(e) => setSearchValue(e.target.value)}
+// 			   value={searchValue}
+// 			   placeholder={"Найти..."}
+// 		/><p/>
+//
+// 		{modeSearch ?
+//
+// 			// режим поиска юзера
+// 			searchPeople.map((u, i) =>
+// 				<div key={i}>
+// 					<div className={usersCss.item1}>
+// 						<img className={usersCss.photo}
+// 							 src={u.photo}
+// 							 onError={e => {
+// 								 e.target.src = face
+// 								 e.target.onerror = null
+// 							 }}
+// 							 alt=""
+// 						/>
+// 						{u.name}
+//
+// 						{/* + - добавить подписчика*/}
+// 						<button
+// 							className={usersCss.button}
+// 							onClick={() => {
+// 								api.setFollow(u.id, true).then((data) => {
+// 									console.log(data)
+// 									if (data.result) {
+// 										let newUsers = []
+// 										let incrementUser = u.id
+//
+// 										props.setUsers(prev => {
+// 											return [...prev, {
+// 												id: u.id,
+// 												photo: u.photo,
+// 												name: u.name,
+// 												messages: {
+// 													allMessages: data.messages.allMessages,
+// 													counterLastMessage: data.messages.counterLastMessage,
+// 													lengthMessages: data.messages.lengthMessages
+// 												}
+// 											}]
+// 										})
+// 										searchPeople.map(u => {
+// 											if (u.id !== incrementUser) {
+// 												newUsers.push(u)
+// 											}
+// 											return newUsers
+// 										})
+// 										setSearchPeople(newUsers)
+// 										props.setAuthorizeCountUsers(pre => pre + 1)
+//
+// 									} else {
+// 										alert("Вы уже подписаны")
+// 									}
+//
+// 								})
+// 							}}
+// 						>+
+// 						</button>
+// 					</div>
+// 				</div>
+// 			)
+// 			:
+//
+// 			// режим общения с подписчиками:
+// 			props.users.map(u =>
+// 				<div onMouseLeave={() => setId(undefined)}
+// 					 className={(props.clickUser === u.id) ? usersCss.clickUser : (id === u.id) ? usersCss.dark : ""}
+// 					 key={u.id}>
+// 					<div className={usersCss.item}>
+// 						<div onClick={() => {
+// 							if (props.clickUser !== u.id) {
+// 								props.setClickUser(u.id)
+// 							}
+// 						}}
+// 							 onMouseEnter={() => setId(u.id)}
+// 							 className={usersCss.item1}>
+// 							<div>
+//
+// 								<img className={usersCss.photo}
+// 									 src={u.photo}
+// 									 alt=''
+// 									 onError={e => {
+// 										 e.target.src = face
+// 										 e.target.onerror = null
+// 									 }}
+// 								/>
+//
+// 							</div>
+// 							<div>
+// 								<div>{u.name}</div>
+//
+// 								{props.usersConnection.includes(u.id) ? <div>В сети</div> : <></>}
+//
+// 								{u.messages.allMessages[u.messages.allMessages.length - 1].post ?
+// 									<div
+// 										className={usersCss.item1122}>{u.messages.allMessages[u.messages.allMessages.length - 1].fromId === props.authorizeId ? "Я:" : ""} {u.messages.allMessages[u.messages.allMessages.length - 1].post}</div>
+// 									:
+// 									<div className={usersCss.item1122}>Нет сообщений</div>}
+// 							</div>
+//
+// 							<div
+// 								className={usersCss.item1123}>{
+// 								props.clickUser!==u.id?
+// 									u.messages.lengthMessages - u.messages.counterLastMessage > 0 ? u.messages.lengthMessages - u.messages.counterLastMessage : ''
+// 									:
+// 									""
+// 							}</div>
+//
+//
+// 						</div>
+//
+// 						{/*{ х - удаление подписчика}*/}
+// 						{(id === u.id) ? <button className={usersCss.button}
+// 												 onClick={() => {
+// 													 api.setFollow(u.id, false).then(() => {
+// 														 let newUsers = []
+// 														 let decrementUser = u.id
+// 														 props.users.map(u => {
+// 															 if (u.id !== decrementUser) {
+// 																 newUsers.push(u)
+// 															 }
+// 															 return newUsers
+// 														 })
+// 														 props.setUsers(newUsers)
+// 														 props.setAuthorizeCountUsers(pre => pre - 1)
+// 													 })
+// 												 }}
+// 						>X</button> : <></>}
+// 					</div>
+// 				</div>)
+// 		}
+// 		<p/>
+//
+// 		{/*кнопка показать ещё:*/}
+// 		{modeSearch ? <></>
+// 			:
+// 			((Math.ceil(props.authorizeCountUsers / 5)) > props.currentPage) ? <div onClick={() => {
+// 				props.setCurrentPage(per => per + 1)
+// 			}}> еще...</div> : <></>
+// 		}
+//
+// 	</div>
+// }
+//
+// export default UsersList
+
+
+import React, {useState} from "react"
 import usersCss from "./users.module.css"
 import face from "../head/img/user.png"
 import * as api from "../../api/api"
+import search from "../users/img/search.png"
+import error from "../users/img/error.png"
 
 const UsersList = (props) => {
 
-	const [id, setId] = useState(undefined)
-	const [searchValue, setSearchValue] = useState("")
-	const [searchPeople, setSearchPeople] = useState([])
-	const [modeSearch, setModeSearch] = useState(true)
+    const [id, setId] = useState(undefined)
+    const [searchValue, setSearchValue] = useState("")
+    const [searchPeople, setSearchPeople] = useState([])
+    const [modeSearch, setModeSearch] = useState(false)
+
+    return <div>
+
+        {/*строка поиска*/}
+        <input onChange={(e) => setSearchValue(e.target.value)}
+               value={searchValue}
+               placeholder={"Найти..."}
+        />
+        <img className={usersCss.buttonSearch}
+             src={modeSearch === true ? error : search}
+
+			 onClick={() => {
+             	if (!modeSearch) { api.getSearchUsers(1, searchValue).then(data => {
+					setModeSearch(true)
+					setSearchPeople(data.newUsers)
+				})}
+
+				 if (modeSearch){setModeSearch(false)}
+
+             }}
+         alt={""}/>
+        <p/>
+
+        {modeSearch ?
+
+            // режим поиска юзера
+
+            searchPeople.map((u, i) =>
+                <div key={i}>
+                    <div className={usersCss.item1}>
+                        <img className={usersCss.photo}
+                             src={u.photo}
+                             onError={e => {
+                                 e.target.src = face
+                                 e.target.onerror = null
+                             }}
+                             alt=""
+                        />
+                        {u.name}
+
+                        {/* + - добавить подписчика*/}
+                        <button
+                            className={usersCss.button}
+                            onClick={() => {
+                                api.setFollow(u.id, true).then((data) => {
+                                    console.log(data)
+                                    if (data.result) {
+                                        let newUsers = []
+                                        let incrementUser = u.id
+
+                                        props.setUsers(prev => {
+                                            return [...prev, {
+                                                id: u.id,
+                                                photo: u.photo,
+                                                name: u.name,
+                                                messages: {
+                                                    allMessages: data.messages.allMessages,
+                                                    counterLastMessage: data.messages.counterLastMessage,
+                                                    lengthMessages: data.messages.lengthMessages
+                                                }
+                                            }]
+                                        })
+                                        searchPeople.map(u => {
+                                            if (u.id !== incrementUser) {
+                                                newUsers.push(u)
+                                            }
+                                            return newUsers
+                                        })
+                                        setSearchPeople(newUsers)
+                                        props.setAuthorizeCountUsers(pre => pre + 1)
+
+                                    } else {
+                                        alert("Вы уже подписаны")
+                                    }
+
+                                })
+                            }}
+                        >+
+                        </button>
+                    </div>
+                </div>
+            )
 
 
-// эффект при вводе символа в поисковой строке:
-	useEffect(() => searchValue !== '' ?
-		api.getSearchUsers(1, searchValue).then(data => {
-			setSearchPeople(data.newUsers)
-			data.newUsers.length === 0 ? setModeSearch(false) : setModeSearch(true)
-		})
-		:
-		setModeSearch(false), [searchValue])
+            :
 
-	return <div>
+            // режим общения с подписчиками:
+            props.users.map(u =>
+                <div onMouseLeave={() => setId(undefined)}
+                     className={(props.clickUser === u.id) ? usersCss.clickUser : (id === u.id) ? usersCss.dark : ""}
+                     key={u.id}>
+                    <div className={usersCss.item}>
+                        <div onClick={() => {
+                            if (props.clickUser !== u.id) {
+                                props.setClickUser(u.id)
+                            }
+                        }}
+                             onMouseEnter={() => setId(u.id)}
+                             className={usersCss.item1}>
+                            <div>
 
-		{/*строка поиска*/}
-		<input onChange={(e) => setSearchValue(e.target.value)}
-			   value={searchValue}
-			   placeholder={"Найти..."}
-		/><p/>
+                                <img className={usersCss.photo}
+                                     src={u.photo}
+                                     alt=''
+                                     onError={e => {
+                                         e.target.src = face
+                                         e.target.onerror = null
+                                     }}
+                                />
 
-		{modeSearch ?
+                            </div>
+                            <div>
+                                <div>{u.name}</div>
 
-			// режим поиска юзера
-			searchPeople.map((u, i) =>
-				<div key={i}>
-					<div className={usersCss.item1}>
-						<img className={usersCss.photo}
-							 src={u.photo}
-							 onError={e => {
-								 e.target.src = face
-								 e.target.onerror = null
-							 }}
-							 alt=""
-						/>
-						{u.name}
+                                {props.usersConnection.includes(u.id) ? <div>В сети</div> : <></>}
 
-						{/* + - добавить подписчика*/}
-						<button
-							className={usersCss.button}
-							onClick={() => {
-								api.setFollow(u.id, true).then((data) => {
-									console.log(data)
-									if (data.result) {
-										let newUsers = []
-										let incrementUser = u.id
+                                {u.messages.allMessages[u.messages.allMessages.length - 1].post ?
+                                    <div
+                                        className={usersCss.item1122}>{u.messages.allMessages[u.messages.allMessages.length - 1].fromId === props.authorizeId ? "Я:" : ""} {u.messages.allMessages[u.messages.allMessages.length - 1].post}</div>
+                                    :
+                                    <div className={usersCss.item1122}>Нет сообщений</div>}
+                            </div>
 
-										props.setUsers(prev => {
-											return [...prev, {
-												id: u.id,
-												photo: u.photo,
-												name: u.name,
-												messages: {
-													allMessages: data.messages.allMessages,
-													counterLastMessage: data.messages.counterLastMessage,
-													lengthMessages: data.messages.lengthMessages
-												}
-											}]
-										})
-										searchPeople.map(u => {
-											if (u.id !== incrementUser) {
-												newUsers.push(u)
-											}
-											return newUsers
-										})
-										setSearchPeople(newUsers)
-										props.setAuthorizeCountUsers(pre => pre + 1)
-
-									} else {
-										alert("Вы уже подписаны")
-									}
-
-								})
-							}}
-						>+
-						</button>
-					</div>
-				</div>
-			)
-			:
-
-			// режим общения с подписчиками:
-			props.users.map(u =>
-				<div onMouseLeave={() => setId(undefined)}
-					 className={(props.clickUser === u.id) ? usersCss.clickUser : (id === u.id) ? usersCss.dark : ""}
-					 key={u.id}>
-					<div className={usersCss.item}>
-						<div onClick={() => {
-							if (props.clickUser !== u.id) {
-							props.setClickUser(u.id)
-							}
-						}}
-							 onMouseEnter={() => setId(u.id)}
-							 className={usersCss.item1}>
-							<div>
-
-								<img className={usersCss.photo}
-									 src={u.photo}
-									 alt=''
-									 onError={e => {
-										 e.target.src = face
-										 e.target.onerror = null
-									 }}
-								/>
-
-							</div>
-							<div>
-								<div>{u.name}</div>
-
-								{props.usersConnection.includes(u.id) ? <div>В сети</div> : <></>}
-
-								{u.messages.allMessages[u.messages.allMessages.length - 1].post ?
-									<div
-										className={usersCss.item1122}>{u.messages.allMessages[u.messages.allMessages.length - 1].fromId === props.authorizeId ? "Я:" : ""} {u.messages.allMessages[u.messages.allMessages.length - 1].post}</div>
-									:
-									<div className={usersCss.item1122}>Нет сообщений</div>}
-							</div>
-
-							<div
-								className={usersCss.item1123}>{
-									props.clickUser!==u.id?
-									u.messages.lengthMessages - u.messages.counterLastMessage > 0 ? u.messages.lengthMessages - u.messages.counterLastMessage : ''
-								:
-										""
-								}</div>
+                            <div
+                                className={usersCss.item1123}>{
+                                props.clickUser !== u.id ?
+                                    u.messages.lengthMessages - u.messages.counterLastMessage > 0 ? u.messages.lengthMessages - u.messages.counterLastMessage : ''
+                                    :
+                                    ""
+                            }</div>
 
 
-						</div>
+                        </div>
 
-						{/*{ х - удаление подписчика}*/}
-						{(id === u.id) ? <button className={usersCss.button}
-												 onClick={() => {
-													 api.setFollow(u.id, false).then(() => {
-														 let newUsers = []
-														 let decrementUser = u.id
-														 props.users.map(u => {
-															 if (u.id !== decrementUser) {
-																 newUsers.push(u)
-															 }
-															 return newUsers
-														 })
-														 props.setUsers(newUsers)
-														 props.setAuthorizeCountUsers(pre => pre - 1)
-													 })
-												 }}
-						>X</button> : <></>}
-					</div>
-				</div>)
-		}
-		<p/>
+                        {/*{ х - удаление подписчика}*/}
+                        {(id === u.id) ? <button className={usersCss.button}
+                                                 onClick={() => {
+                                                     api.setFollow(u.id, false).then(() => {
+                                                         let newUsers = []
+                                                         let decrementUser = u.id
+                                                         props.users.map(u => {
+                                                             if (u.id !== decrementUser) {
+                                                                 newUsers.push(u)
+                                                             }
+                                                             return newUsers
+                                                         })
+                                                         props.setUsers(newUsers)
+                                                         props.setAuthorizeCountUsers(pre => pre - 1)
+                                                     })
+                                                 }}
+                        >X</button> : <></>}
+                    </div>
+                </div>)
+        }
+        <p/>
 
-		{/*кнопка показать ещё:*/}
-		{modeSearch ? <></>
-			:
-			((Math.ceil(props.authorizeCountUsers / 5)) > props.currentPage) ? <div onClick={() => {
-				props.setCurrentPage(per => per + 1)
-			}}> еще...</div> : <></>
-		}
+        {/*кнопка показать ещё:*/}
+        {modeSearch ? <></>
+            :
+            ((Math.ceil(props.authorizeCountUsers / 5)) > props.currentPage) ? <div onClick={() => {
+                props.setCurrentPage(per => per + 1)
+            }}> еще...</div> : <></>
+        }
 
-	</div>
+    </div>
 }
 
 export default UsersList
